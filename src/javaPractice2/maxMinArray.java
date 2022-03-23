0805618c83096382c7813d6e1e60d0d037d0d3a7
Anik <Anik6989@gmail.com>
2022-03-23 17:43:41 -0400

    updated 2nd commit

--- src/javaPractice2/maxMinArray.java
+++ src/javaPractice2/maxMinArray.java
@@ -2,26 +2,35 @@ package javaPractice2;
 
 public class maxMinArray {
 	
-	
-	public static void main() {
-	
-	int a[]={4,7,6,4,8,9};
-	
-	int max=a[0];
-	
-	for(int i=1;i<a.length;i++){
+	public static void main(String[]args) {
 		
-	if(a[i]>max) {
-	
-	max=a[i];
-	}
-	
-	
-	
-	}
-	
-	System.out.println(max);
-	
+		int a[]={4,7,6,4,8,9};
+		
+		
+		int max=a[0];
+		
+		int min = a[0];
+		
+		for(int i=1;i<a.length;i++){
+			
+		if(a[i]>max) {
+		
+		max=a[i];
+		}
+		if(a[i]<min) {
+			
+			min=a[i];
+			}
+		
+		
+		}
+		
+		System.out.println("maximum valie is: " + max);
+		System.out.println("minimum valie is: " + min);
+		
+		}
 	}
 
-}
+
+	
+
